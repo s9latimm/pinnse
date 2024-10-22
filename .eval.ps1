0b110ed8bff5640205701a9c51c27397b0b7544f
Lauritz Timm <44357397+s9latimm@users.noreply.github.com>
2024-10-22 09:57:19 +0200

    Update eval

--- .eval.ps1
+++ .eval.ps1
@@ -1,29 +1,29 @@
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/empty__0_100__0_020__0_500"  --nu 0.02 --inlet .5
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/empty__0_100__0_010__1_000"  --nu 0.01 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/empty__0_100__0_020__1_000"  --nu 0.02 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/empty__0_100__0_020__2_000"  --nu 0.02 --inlet 2
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/empty__0_100__0_040__1_000"  --nu 0.04 --inlet 1
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/1.1" --nu 0.02  --inlet .5 # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/1.2" --nu 0.01  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/1.3" --nu 0.02  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/1.4" --nu 0.02  --inlet 2  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E empty  --id "eval_5/1.5" --nu 0.04  --inlet 1  # --dry
 
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/step__0_100__0_010__0_500"   --nu 0.01  --inlet .5
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/step__0_100__0_005__1_000"   --nu 0.005 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/step__0_100__0_010__1_000"   --nu 0.01  --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/step__0_100__0_010__2_000"   --nu 0.01  --inlet 2
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/step__0_100__0_020__1_000"   --nu 0.02  --inlet 1
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/2.1" --nu 0.01  --inlet .5 # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/2.2" --nu 0.005 --inlet 1  # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/2.3" --nu 0.01  --inlet 1  # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/2.4" --nu 0.01  --inlet 2  # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E step   --id "eval_5/2.5" --nu 0.02  --inlet 1  # --dry
 
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/slalom__0_100__0_010__0_500" --nu 0.01  --inlet .5
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/slalom__0_100__0_005__1_000" --nu 0.005 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/slalom__0_100__0_010__1_000" --nu 0.01  --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/slalom__0_100__0_010__2_000" --nu 0.01  --inlet 2
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/slalom__0_100__0_020__1_000" --nu 0.02  --inlet 1
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/3.1" --nu 0.01  --inlet .5 # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/3.2" --nu 0.005 --inlet 1  # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/3.3" --nu 0.01  --inlet 1  # --dry
+#python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/3.4" --nu 0.01  --inlet 2  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slalom --id "eval_5/3.5" --nu 0.02  --inlet 1  # --dry
 
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/block__0_100__0_020__0_500"  --nu 0.02 --inlet .5
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/block__0_100__0_010__1_000"  --nu 0.01 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/block__0_100__0_020__1_000"  --nu 0.02 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/block__0_100__0_020__2_000"  --nu 0.02 --inlet 2
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/block__0_100__0_040__1_000"  --nu 0.04 --inlet 1
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/4.1" --nu 0.02  --inlet .5 # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/4.2" --nu 0.01  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/4.3" --nu 0.02  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/4.4" --nu 0.02  --inlet 2  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E block  --id "eval_5/4.5" --nu 0.04  --inlet 1  # --dry
 
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/slit__0_100__0_020__0_500"   --nu 0.02 --inlet .5
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/slit__0_100__0_010__1_000"   --nu 0.01 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/slit__0_100__0_020__1_000"   --nu 0.02 --inlet 1
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/slit__0_100__0_020__2_000"   --nu 0.02 --inlet 2
-python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/slit__0_100__0_040__1_000"   --nu 0.04 --inlet 1
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/5.1" --nu 0.02  --inlet .5 # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/5.2" --nu 0.01  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/5.3" --nu 0.02  --inlet 1  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/5.4" --nu 0.02  --inlet 2  # --dry
+python -m src.nse -D cuda -PFGN 200000 -L 150:150:150 -E slit   --id "eval_5/5.5" --nu 0.04  --inlet 1  # --dry
